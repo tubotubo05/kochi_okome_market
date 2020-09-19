935ac840c67ef02be2c009403ef905932adfdd6f
tubotubo05 <69897229+tubotubo05@users.noreply.github.com>
2020-09-19 16:53:13 +0900

    commit3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
       username == Rails.application.credentials[:basic_auth][:user] &&
       password == Rails.application.credentials[:basic_auth][:pass]
     end
+  end
 
-    def production?
-      Rails.env.production?
-    end
+  def production?
+    Rails.env.production?
   end
 
 end
