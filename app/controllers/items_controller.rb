781fb6d0dd316090cef84f4a3390e1ad00100798
kyo9150 <69196095+kyo9150@users.noreply.github.com>
2020-10-08 20:39:38 +0900

    add

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+<<<<<<< Updated upstream
+=======
+
+  def confirm
+    
+  end
+  
+  def show
+    render:confirm
+  end
+  
+
+  def new
+  end
+  
+  def create
+  end
+>>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'items#index'
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+
+  resources :items, only: [:index, :new, :create, :show]
+  post "items/confirm" => "items#confirm"
+>>>>>>> Stashed changes
 end
