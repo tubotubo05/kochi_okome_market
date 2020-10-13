c69a0c63201c50cad74c5845648d80823dae9179
kyo9150 <69196095+kyo9150@users.noreply.github.com>
2020-10-13 16:08:00 +0900

    index_show

---
+++ app/assets/javascripts/images.js
@@ -0,0 +1,19 @@
+$(document).on('turbolinks:load', function(){
+  // $('.carousel').slick({
+  //  });
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    //dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+});
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items, dependent: :destroy
+  has_many :comments,dependent: :destroy
   
 end
