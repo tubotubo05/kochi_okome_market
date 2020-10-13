275cd477d32b8d16c76afd75b53819ccdcae236e
kyo9150 <69196095+kyo9150@users.noreply.github.com>
2020-10-13 16:33:10 +0900

    comment out

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,6 +1,4 @@
 $(document).on('turbolinks:load', function(){
-  // $('.carousel').slick({
-  //  });
   $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -12,7 +10,6 @@ $(document).on('turbolinks:load', function(){
     slidesToShow: 3,
     slidesToScroll: 1,
     asNavFor: '.slider-for',
-    //dots: true,
     centerMode: true,
     focusOnSelect: true
   });
